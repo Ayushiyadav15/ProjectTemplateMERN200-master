bf6f3702edf83c6599c24274aa87805620441280
Ayushiyadav15 <ayushiyadav2222>
2022-04-21 14:44:39 +0530

    ..

---
+++ frontend/src/components/main/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const login = () => {
+  return( 
+<div>
+<div class="Mycard">
+    <p class="sign">login here</p>
+    <input class ="myinput" placeholder="Email" type="email "/>
+      <input class="myinput" placeholder="password" type="password" />
+    <button class="mybutton">Submit</button>
+    </div>
+</div>
+  
+  )};
+
+export default login;
